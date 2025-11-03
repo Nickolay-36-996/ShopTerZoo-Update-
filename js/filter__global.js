6d1efdad2b07aeaa033a37dda50f796fde8ff98a
Nickolay-36-996 <trained.educated879@gmail.com>
2025-11-03 04:12:46 +0300

    message

--- js/filter__global.js
+++ js/filter__global.js
@@ -21,21 +21,54 @@ document.addEventListener("DOMContentLoaded", () => {
   window.animalFilter = function (animalList) {
     const animalContainer = document.getElementById("animals-list");
 
-    for (const category of animalList) {
-      const animalItem = document.createElement("a");
-      animalItem.className = "animal__category__catalog";
-      animalItem.href = `catalog.html?animal=${category.type.toLowerCase()}`;
-      animalItem.innerHTML = `
+    const isCatalogPage = window.location.pathname.includes("catalog.html");
+    const isIndexPage = window.location.pathname.includes("index.html");
+
+    if (isIndexPage) {
+      for (const category of animalList) {
+        const animalItem = document.createElement("a");
+        animalItem.className = "animal__category__catalog";
+        animalItem.href = `catalog.html?animal=${category.type.toLowerCase()}`;
+        animalItem.innerHTML = `
             <img src="${category.image}" alt="${category.title}">
             <p class="animal__category__catalog__title">${category.type}</p>
             `;
 
-      animalContainer.appendChild(animalItem);
+        animalContainer.appendChild(animalItem);
 
-      animalItem.addEventListener("click", function () {
-        const selectedCategory = category.type.toLowerCase();
-        localStorage.setItem("autoFilterCategory", selectedCategory);
-      });
+        animalItem.addEventListener("click", function () {
+          const selectedCategory = category.type.toLowerCase();
+          localStorage.setItem("autoFilterCategory", selectedCategory);
+        });
+      }
+    } else if (isCatalogPage) {
+      for (const category of animalList) {
+        const animalItem = document.createElement("button");
+        animalItem.className = "animal__category__catalog";
+        animalItem.innerHTML = `
+            <img src="${category.image}" alt="${category.title}">
+            <p class="animal__category__catalog__title">${category.type}</p>
+            `;
+
+        animalContainer.appendChild(animalItem);
+
+        animalItem.addEventListener("click", function () {
+          const selectedCategory = category.type.toLowerCase();
+          const filterItems = document.querySelectorAll(
+            ".products__catalog__filter__type__list__item"
+          );
+
+          for (let filter of filterItems) {
+            const filterText = filter
+              .querySelector(".products__catalog__filter__type__txt")
+              .textContent.toLowerCase();
+            if (filterText === selectedCategory) {
+              filter.click();
+              break;
+            }
+          }
+        });
+      }
     }
   };
 });
