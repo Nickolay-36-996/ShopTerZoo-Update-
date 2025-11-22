ca81dcdab7677baef72e0155b43b886c1230a3bd
Nickolay-36-996 <trained.educated879@gmail.com>
2025-11-23 02:50:19 +0300

    message

--- js/rest__filter__catalog.js
+++ js/rest__filter__catalog.js
@@ -11,5 +11,33 @@ window.promotionalFilter = function () {
     if (indicator) {
       indicator.classList.toggle("promotional__item__indicator__active");
     }
+
+    const isActive = indicator.classList.contains(
+      "promotional__item__indicator__active"
+    );
+
+    let url =
+      "https://oliver1ck.pythonanywhere.com/api/get_products_filter/?order=date_create";
+
+    if (isActive) {
+      url += "&sale__percent__gt=0";
+    }
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error(`HTTP status: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => {
+        if (typeof window.productItems === "function") {
+          window.productItems(data.results);
+        }
+        if (typeof window.updatePagination === "function") {
+          window.updatePagination(data, 1, url.replace("&page=1", ""));
+        }
+      })
+      .catch((error) => {
+        console.error("Ошибка загрузки товаров по акции:", error);
+      });
   });
 };
